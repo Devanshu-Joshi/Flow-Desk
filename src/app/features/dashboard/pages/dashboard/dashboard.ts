3f6bd50e0f8cd5b56072b0b70d44b1d0a9b3b9b3
Devanshu Joshi <iamdevanshu007@gmail.com>
2026-01-10 17:43:53 +0530

    feat: implement search functionality in dashboard with debounce for improved performance

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
-import { Component, inject, Signal } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { Component, inject, OnInit, Signal } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { TaskService } from '../../../../core/services/task';
-import { computed } from '@angular/core';
+import { signal, computed } from '@angular/core';
 import { Task } from '../../../../core/models/Task';
 import { PostService } from '../../../../core/services/post';
+import { debounceTime } from 'rxjs';
 
 export type TaskStatus = 'Incomplete' | 'Completed' | 'InProgress';
 @Component({
@@ -12,7 +13,7 @@ export type TaskStatus = 'Incomplete' | 'Completed' | 'InProgress';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
   isDialogClosed: boolean = true;
   toggleDialog() {
     this.isDialogClosed = !this.isDialogClosed;
@@ -20,9 +21,32 @@ export class Dashboard {
 
   fb = inject(FormBuilder);
   tasks!: Signal<Task[]>;
+  searchControl = new FormControl('');
+  searchTerm = signal('');
+  filteredTasks = computed(() => {
+    const term = this.searchTerm().toLowerCase();
+
+    if (!term) {
+      return this.tasks();
+    }
+
+    return this.tasks().filter(task =>
+      // task.title.toLowerCase().includes(term) ||
+      // task.status.toLowerCase().includes(term)
+      task.title.toLowerCase().includes(term)
+    );
+  });
 
   constructor(public taskService: TaskService, private postService: PostService) {
     this.tasks = this.taskService.tasks;
+  }
+
+  ngOnInit() {
+    this.searchControl.valueChanges
+      .pipe(debounceTime(300))
+      .subscribe(value => {
+        this.searchTerm.set(value || '');
+      });
   }
 
   totalTasks = computed(() => this.tasks().length);
@@ -43,6 +67,7 @@ export class Dashboard {
     dueDate: ['', Validators.required],
     status: ['Incomplete' as TaskStatus, Validators.required]
   });
+
 
   async submit() {
     if (this.taskForm.invalid) {
