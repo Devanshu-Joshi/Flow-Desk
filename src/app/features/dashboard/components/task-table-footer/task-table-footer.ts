11bc4684c343b47c77080797125cfe6ad09d9492
Devanshu Joshi <iamdevanshu007@gmail.com>
2026-01-15 18:17:15 +0530

    Responsive changes

--- src/app/features/dashboard/components/task-table-footer/task-table-footer.ts
+++ src/app/features/dashboard/components/task-table-footer/task-table-footer.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
-  selector: 'tr[app-task-table-footer]',
+  selector: 'app-task-table-footer',
   imports: [CommonModule, NgxPaginationModule],
   templateUrl: './task-table-footer.html',
   styleUrl: './task-table-footer.css',
@@ -11,7 +11,6 @@ import { NgxPaginationModule } from 'ngx-pagination';
 export class TaskTableFooter {
   @Input() total: number = 0;
   @Input() itemsPerPage: number = 5;
-  @Input() colspan: number = 5;
 
   @Output() clearFilters = new EventEmitter<void>();
   @Output() pageChange = new EventEmitter<number>();
