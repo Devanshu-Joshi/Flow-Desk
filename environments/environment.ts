2900ae9a7f788998f7bd2ef232d44426c207e8aa
Devanshu Joshi <iamdevanshu007@gmail.com>
2026-02-13 12:14:14 +0530

    TEMP: Update API_URL in environment configuration for correct endpoint

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    API_URL: 'https://kind-eggs-cut.loca.lt/api',
+    API_URL: 'https://mug-length-gabriel-minute.trycloudflare.com/api',
     // API_URL: 'http://localhost:3080/api',
     production: false
 };
