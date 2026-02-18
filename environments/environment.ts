ee1e1a316354009148f854ad1383e2550eedbfbb
Devanshu Joshi <iamdevanshu007@gmail.com>
2026-02-18 10:33:55 +0530

    fix(environment): update API_URL to correct endpoint for development

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    API_URL: 'https://boost-rfc-attempting-rocket.trycloudflare.com/api',
+    API_URL: 'https://sword-zip-mileage-lines.trycloudflare.com/api',
     // API_URL: 'http://localhost:3080/api',
     production: false
 };
